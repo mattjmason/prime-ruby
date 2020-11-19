dcc6e76720bd906c70b0de92cb1981d87ffb64b7
Matt Mason <masonmj10@gmail.com>
2020-11-19 02:08:42 -0500

    commit tree b91f45e43a26a905c8cebd0bc06e705558b92800

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+
+
+def prime?(number)
+    if number <= 1 
+        return false
+    elsif number <= 3
+        return true
+    else (2..number/2).none? do |int|
+        number % int == 0
+    end 
+end 
+end
